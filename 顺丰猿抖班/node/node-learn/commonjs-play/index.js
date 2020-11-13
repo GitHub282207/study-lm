75393835ee6010078c9db2811e476be107d652ea
GitHub282207 <2676890377@qq.com>
2020-11-13 16:59:39 +0800

    修改

--- 顺丰猿抖班/node/node-learn/commonjs-play/index.js
+++ 顺丰猿抖班/node/node-learn/commonjs-play/index.js
@@ -1,5 +1,20 @@
-var playerAction = process.argv[process.argv.length-1]
+// var playerAction = process.argv[process.argv.length-1]
 const game = require('./lib')//game是抛出的那个函数
 
-const result = game(playerAction)
-console.log(result);
+// const result = game(playerAction)
+// console.log(result);
+
+// 获取进程的标准输入
+let count = 0
+process.stdin.on('data',e=>{
+    const playerAction = e.toString().trim()
+    const result = game(playerAction)
+    console.log(result);
+    if(result == -1){
+        count++
+    }
+    if(count === 3){
+        console.log('你太厉害了，不跟你玩儿了');
+        process.exit()//结束进程
+    }
+})
