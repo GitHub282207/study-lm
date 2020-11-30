2b873b972567baac676fece4edb3501f2a61f85c
GitHub282207 <2676890377@qq.com>
2020-11-30 22:10:28 +0800

    正则

---
+++ 顺丰猿抖班/vue/vue-elm/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header'
+
+export default {
+  components:{
+    'v-header':header
+  }
+}
+</script>
+
+<style>
+
+</style>
